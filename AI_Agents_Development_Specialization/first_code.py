97ae3d11c337e02a5cd173327cf304acd39ea4e7
rickrribeiro <rickribeiro99@gmail.com>
2026-02-03 09:28:25 -0300

    first course codes

---
+++ AI_Agents_Development_Specialization/first_code.py
@@ -0,0 +1,46 @@
+import json
+import os
+from dotenv import load_dotenv
+from litellm import completion
+
+load_dotenv()
+
+def generate_response(messages):
+    response = completion(
+        model="gemini/gemini-3-flash-preview", 
+        messages=messages
+    )
+    return response.choices[0].message.content
+
+countries = {
+    'name1': 'France',
+    'name2': 'Germany',
+}
+
+messages = [
+        # {"role": "system", "content": "Answer in Base64 format"},
+        {"role": "user", "content": f"whats the capital of these countries {json.dumps(countries)}?"}
+        ]
+
+response = generate_response(messages)
+print("--------- RESPONSE 1 ---------   ")
+print(response)
+
+messages = [
+        {"role": "system", "content": "Answer in Base64 format"},
+        {"role": "user", "content": f"whats the capital of these countries {json.dumps(countries)}?"}
+        ]
+
+response = generate_response(messages)
+
+print("--------- RESPONSE 2 ---------   ")
+print(response)
+
+
+messages = [
+        {"role": "user", "content": f"Cand you send to me again the capitals?"}
+        ]
+
+response = generate_response(messages)
+print("--------- RESPONSE 3 ---------   ")
+print(response)
